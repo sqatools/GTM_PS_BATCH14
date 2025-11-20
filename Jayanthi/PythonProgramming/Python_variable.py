a9bcfc6ff41515ea02d69c3bf599fd337ae680a5
Jayanthi Ajay <jayanthi.j90@gmail.com>
2025-11-20 22:22:12 +0530

    Python Variable class 5 arithmetric operator

---
+++ Jayanthi/PythonProgramming/Python_variable.py
@@ -0,0 +1,69 @@
+# Variable
+a=10
+print("Value of a is", a)
+
+print('_'*50)
+# header(memory)
+print(a,id(a))
+d=10
+print(d,id(d))
+
+print('_'*50)
+x=y=z=10
+p,q,r=70,80,90
+print("Value of x",x)
+print("Value of x",y)
+print("Value of x",z)
+print("Value of x",p)
+print("Value of x",q)
+print("Value of x",r)
+
+print('_'*50)
+# Add
+m=6
+n=2
+o=m+n
+print("Value of m+n",o)
+print("value of m+n",m+n)
+
+print('_'*50)
+# Sub
+print("Value of m-n", m-n)
+
+print('_'*50)
+# Mul
+print("Value of m*n", m*n)
+
+print('_'*50)
+# div with decimal
+print("Value of m*n", m*n)
+
+print('_'*50)
+# div without decimal
+print("Value of m*n", m*n)
+
+print('_'*50)
+# power
+print("Value of m power n", m**n)
+
+print('_'*50)
+# % remainder
+print("Value of m%n", m%n)
+
+print('_'*50)
+# case sensitive
+name = "jayanthi"
+Name ="Janvika"
+NAME="AJAY"
+naMe="Pari"
+print("Value of name", name)
+print("Value of name", Name)
+print("Value of name", NAME)
+print("Value of name", naMe)
+
+
+
+
+
+
+
