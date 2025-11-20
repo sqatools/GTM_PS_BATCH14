1845d55dbb8e66fa21a7e0dc18b5fb387129afb9
nalinimajeti <nmajeticollegecraft@gmail.com>
2025-11-20 22:00:23 +0530

    second python program

---
+++ Nalini/PythonPrograms/python_variables.py
@@ -0,0 +1,9 @@
+a=10
+b=20
+print("value of a":, a)
+print("value of b", b)
+print("address of a", id(a))
+x=y=z=50
+#assign the 50 at a time to three variables
+p,q,r=70,80,90
+# assign three different value to 3 variables at a time
