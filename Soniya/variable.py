bf30f89ccf9a48519abfc2c4d549edba7e0486eb
sonikavi <soniyakavya8@gmail.com>
2025-11-20 22:02:32 +0530

    "commit_variable"

--- Soniya/variable.py
+++ Soniya/variable.py
@@ -1,6 +1,6 @@
 #### Assign three different value three variable in one go #####
-p, q, r = 70, 80, 90
-print("value of p :", p) # 70
-print("value of q :", q) # 80
-print("value of r :", r) # 90
+a, b, c = 700, 800, 900
+print("value of p :", a) # 700
+print("value of q :", b) # 800
+print("value of r :", c) # 900
 
