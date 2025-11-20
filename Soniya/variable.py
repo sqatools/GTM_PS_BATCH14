d7738be6cf8b7659e502ecf0cdd7767d167cbdac
sonikavi <soniyakavya8@gmail.com>
2025-11-20 22:26:33 +0530

    "commit_variable"

--- Soniya/variable.py
+++ Soniya/variable.py
@@ -5,12 +5,34 @@ print("value of q :", b) # 800
 print("value of r :", c) # 900
 
 # Variable names are case-sensitive.
-name = "Rahul"
-Name = "Rohit"
-NAME = "Raman"
-NAme = "Raghav"
+name = "soniya"
+Name = "nag"
+NAME = "yuvaan"
+NAme = "prahap"
 print(name , "|", Name, "|", NAME, "|", NAme)
-# Rahul | Rohit | Raman | Raghav
+# soniya | nag | yuvaan | prathap
+
+##################
+a=10
+print (a,id(a))
+print("value of a :",a )  # value of a : 10
+
+
+
+#### Assign three different value three variable in one go #####
+p, q, r = 70, 80, 90
+print("value of p :", p) # 70
+print("value of q :", q) # 80
+print("value of r :", r) # 90
+
+
+################
+print("*"*50)
+
+#################
+#######math operation
+
+
 
 
 
