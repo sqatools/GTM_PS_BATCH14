a1984b553842d13d49085d8f5041a8393deaf93b
nalinimajeti <nmajeticollegecraft@gmail.com>
2025-11-21 22:14:30 +0530

    Learning datatypes

---
+++ Nalini/PythonPrograms/python_typeofvariabls.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20.5
+c = 245.45654
+name = 'naliniPavan'
+print("data type of a", type(a))
+
+print("data type of a", type(b))
+
+print("data type of a", type(c))
+
+print("data type of a", type(name))
