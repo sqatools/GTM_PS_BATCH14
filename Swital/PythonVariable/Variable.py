1ab728a6a007f8ea900c70e60ae876ecc2ccbf15
Swital <switalmacwan2207@gmail.com>
2025-11-21 15:34:13 +0530

    "Variable Practice"

---
+++ Swital/PythonVariable/Variable.py
@@ -0,0 +1,23 @@
+# Creating Variables
+a = 10
+name = "Swital"
+price = 99.08
+print("print value", a)
+print("name is", name)
+print("Price of the card", price)
+
+# Assigning Values
+print("_" *60)
+a = 6
+b = 8.9
+c = a+b
+print("Summation", c)
+
+# Addition
+print("_" *60)
+m = 9
+n = 8
+print("Add:", m+n)
+print("Sub:", m-n)
+print("Multiply:", m*n)
+print("Divide:", m/n)
