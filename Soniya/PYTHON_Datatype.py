32020edb5edd8c4a0317c6a9c05e79f0f0be5b66
sonikavi <soniyakavya8@gmail.com>
2025-11-21 22:33:53 +0530

    "Data type int,string,float,number"

---
+++ Soniya/PYTHON_Datatype.py
@@ -0,0 +1,83 @@
+##############integer########
+n1 = 0
+n2 = 1
+n3 = 8989
+n4 = 889999
+# n5 = 78.89 | not int
+print("n1 :", n1, type(n1))  # 0 <class 'int'>
+print("n2 :", n2, type(n2))  # 1 <class 'int'>
+print("n3 :", n3, type(n3))  # 7878798798 <class 'int'>
+print("n4 :", n4, type(n4))
+
+
+print("*"*50)
+print("float")
+#########################
+var1 =0.0
+var2 = 5.6
+var3 = 78978.4654654645
+print("var1 :", var1, type(var1))  # 0.0 <class 'float'>
+print("var2:", var2, type(var2))   # 5.6 <class 'float'>
+print("var3 :", var3, type(var3))  # 78978.4654654645 <class 'float'>
+
+##################
+print("&"*50)
+s1 = ""
+s2 = 'Hello'
+s3 = "H"
+
+s4 = """
+Copilot Search Branding
+Global web icon
+Cricbuzz
+https://www.cricbuzz.com
+"""
+
+s5 = '''
+2 days ago · Check live cricket scores,
+ upcoming cricket matches, ball-by-ball commentary,
+  & live score updates with latest news for today's
+   live cricket matches on ESPNcricinfo.
+'''
+
+s6 = "We are learning Python Programming"
+
+
+print("s1 :", s1, type(s1))
+print("_"*15)
+print("s2 :", s2, type(s2))
+print("_"*15)
+print("s3 :", s3, type(s3))
+print("_"*15)
+print("s4 :", s4, type(s4))
+print("_"*15)
+print("s5 :", s5, type(s5))
+print("_"*15)
+print("s6 :", s6, type(s6))
+
+s7 = "ello, we are 6768766 &*&*(&( ##%$# learning Python " \
+     "ProgrammingVery good evening"
+print("s7 :", s7, type(s7))
+
+
+
+###################################################3
+
+s8 = "My teacher's"
+s9 = 'Learning "Python" Programming'
+print("s8 :", s8)  # My teacher's
+print("s9 :", s9)  # Learning "Python" Programming
+
+
+#########################
+abc = "Hello"
+abc = "Python"
+abc = "soniya"
+print(abc)
+
+############################
+"""""""""string indexing"""""""""
+str10="am the python automation tester"
+print (str10[-6])
+
+
