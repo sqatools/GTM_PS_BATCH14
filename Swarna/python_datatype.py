9d9cc2fe4932fef67ae9cd2b284b57e81e2c40b6
swarna <tripathy.swarna7@gmail.com>
2025-11-21 17:03:16 +0000

    python variable program

---
+++ Swarna/python_datatype.py
@@ -0,0 +1,22 @@
+#Indexing
+str10="Python"
+print(str10[0])
+print(str10[-6])
+print(str10[2])
+print(str10[-4])
+print(str10[-4])
+print(str10[(-2)**2])
+
+#
+var1=0.0
+var2=5.6
+var3=78978.46546
+print("var1:",var1,type(var1))
+print("var2:",var2,type(var2))
+print("var3:",var3,type(var3))
+
+#
+p1=10+20j
+print("p1:",p1,type(p1))
+print("Real number:",p1.real)
+print("Imaginary number:",p1)
