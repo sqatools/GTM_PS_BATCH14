5f9dd754def49ba312a628016e638b9577136115
Swital <switalmacwan2207@gmail.com>
2025-11-21 23:59:45 +0530

    Datatype Practice session

--- Swital/PythonDataType/DataType.py
+++ Swital/PythonDataType/DataType.py
@@ -27,8 +27,8 @@ s2 = """Python is a high-level, general-purpose programming language.
 Its design philosophy emphasizes code readability with the use of significant indentation. 
 Python is dynamically type-checked and garbage-collected."""
 print(s2)
-
 #String Postive and negative number
 st3 = "Programming"
 print(st3[4])
 print(st3[-5])
+print(str[9])
