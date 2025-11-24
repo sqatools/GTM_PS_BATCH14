7e1217c35a92e173ae92a23f75cb4a8a8a079691
swarna <tripathy.swarna7@gmail.com>
2025-11-24 16:52:15 +0000

    python variable program

--- Swarna/python_datatype.py
+++ Swarna/python_datatype.py
@@ -19,4 +19,43 @@ print("var3:",var3,type(var3))
 p1=10+20j
 print("p1:",p1,type(p1))
 print("Real number:",p1.real)
-print("Imaginary number:",p1)
+print("Imaginary number:",p1.imag)
+
+#Sequential Data Types
+#String data type
+s1=""
+s2='Hello'
+s3="H"
+s4="""
+copilot search branding"""
+s5='''This is a multi-line
+string.'''
+s6="We are learning python programming."
+
+
+print("s1:",s1,type(s1))
+print("_"*15)
+print("s2:",s2,type(s2))
+print("_"*15)
+print("s3:",s3,type(s3))
+print("_"*15)
+print("s4:",s4,type(s4))
+print("_"*15)
+print("s5:",s5,type(s5))
+print("_"*15)
+print("s6:",s6,type(s6))
+print("_"*15)
+
+
+s7 = "hello, we are 6768766 &*&*(&( ##%$# learning Python " \
+     "ProgrammingVery good evening"
+print("s7:",s7,type(s7))
+
+s8="My teacher's"
+s9='Learning "python" programming '
+print("s8:",s8,type(s8))
+print("s9:",s9,type(s9))
+
+
+
+
