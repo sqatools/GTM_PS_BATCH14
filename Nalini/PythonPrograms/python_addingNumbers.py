3176d0012fa7a8ea5387535ea09e59071de35bb4
nalinimajeti <nmajeticollegecraft@gmail.com>
2025-11-24 15:25:51 +0530

    simple Python Programs

--- Nalini/PythonPrograms/python_addingNumbers.py
+++ Nalini/PythonPrograms/python_addingNumbers.py
@@ -56,8 +56,71 @@ a = 10
 b = 30
 c = 40
 average = (a + b + c) / 3
-print("average is : ", average)
+average1 = (a + b + c) // 3
+print("Average is : ", average)
+print("Average round off: : ", average1)
 print("*" * 50)
 
 
+# 7. Python program to print the square and cube of a given number.
+
+x = 6
+y = 9
+square = x ** 2
+cube = y ** 3
+print("square is : ", square)
+print("cube  is : ", cube)
+print("*" * 50)
+
+# 8 interchange the values between variables.
+x = 20
+y = 30
+x, y = y, x
+print("x and y", x, y)
+print("*" * 50)
+
+# 9 program to solve the given math formula.Pythagorous theorem.
+# Theorem : (a2 + b2 = c2)
+import math
+
+a = 4
+b = 2
+
+c = math.sqrt(a**2 + b**2)
+
+print("Hypotenuse (c) =", c)
+print("*" * 50)
+
+# 10). Python program to solve the given math formula.
+# (a + b)^2 = a^2 + b^2 + 2ab
+
+a = 5
+b = 4
+lhs = (a + b) ** 2
+print (lhs)
+rhs = (a ** 2) + (b ** 2) + (2 * a * b)
+print(rhs)
+print("*" * 50)
+
+# 11. python program to solve the given math formula.
+# Formula : a^2 – b^2 = (a-b)(a+b)
+
+a = 4
+b = 6
+lhs = (a ** 2) - (b ** 2)
+print (lhs)
+rhs = (a - b) * (a + b)
+print(rhs)
+print("*" * 50)
+# 12 Python program to calculate simple interest.
+
+
+princAmount = 3000
+rate = 12
+time = 2
+interest = (princAmount * rate * time) / 100
+
+print("Interest amount:", interest)
+
+print("*" * 50)
 
