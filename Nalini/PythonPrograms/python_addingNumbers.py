bf601b488a47b5fdd9b9ec435c5bf5efeb46fc34
nalinimajeti <nmajeticollegecraft@gmail.com>
2025-11-24 11:48:59 +0530

    Adding Numbers

--- Nalini/PythonPrograms/python_addingNumbers.py
+++ Nalini/PythonPrograms/python_addingNumbers.py
@@ -1,5 +1,14 @@
+# addition of 2 numbers
 num1 = 20
 num2 = 30
 total = num1 + num2
-print ("Total :", total)
+print("Total  :", total)
+num3, num4 = 50, 60
+print("Total num1,num2,num3 and num4 is:", total+num3+num4)
+
+# subtraction of numbers
+x = 60
+y = 70
+print("difference:", x-y)
+
 
