cc662b4fbfe0d22a832e0010655c927b1d14e79d
DeepeshGit25 <satish_paliwal03@yahoo.co.in>
2025-11-27 22:15:42 +0530

    if condition-2

--- Satish/PythonProgramming/if_condition/if_condition.py
+++ Satish/PythonProgramming/if_condition/if_condition.py
@@ -43,4 +43,19 @@ else:
     print("Try next time")
 
 
+# using and, or with if to check multiple conditions together
+
+num=int(input("Enter no"))
+if num%3==0 and num%5==0 :
+    print("num is divisible by both 3 and 5")
+else:
+    print("num is not divisible by both 3 and 5")
+
+print("_"*50)
+num=int(input("Enter no"))
+if num%3==0 or num%5==0 :
+    print("num is divisible by  3 or 5")
+else:
+    print("num is not divisible by both 3 and 5")
+
 
