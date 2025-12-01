a7e59b77665fd6ab3b4fc6ad973e025fe7d7f317
nalinimajeti <nmajeticollegecraft@gmail.com>
2025-12-01 12:21:54 +0530

    simple Python Programs

--- Nalini/PythonPrograms/python_Ifcondition.py
+++ Nalini/PythonPrograms/python_Ifcondition.py
@@ -1,3 +1,4 @@
+"""
 marks = input("enter the Mrks : ")
 x = int(marks)
 if x <= 30:
@@ -7,6 +8,7 @@ else:
 
 print("*"*50)
 """
+""""
 3). If else program to assign grades as per total marks.
 marks > 40: Fail
 marks 40 – 50: grade C
@@ -16,6 +18,7 @@ marks 70 – 80: grade A+
 marks 80 – 90: grade A++
 marks 90 – 100: grade Excellent
 marks > 100: Invalid marks
+"""
 """
 x = input("enter your Marks : ")
 marks = int(x)
@@ -37,7 +40,62 @@ elif marks > 100:
     print ("marks should not be more than 100")
 
 print("*"*50)
+"""
+"""
 # 4). Python program to check the given number divided by 3 and 5.
+
+num = input("Enter your number: ")
+x = int(num)
+
+if x % 3 == 0 and x % 5 == 0:
+    print("number divisible by 3 and 5")
+else:
+    print("number not divisible by 3 and 5")
+ """"""""
+# 5). Python program to print the square of the number if it is divided by 11
+num = input("enter your number")
+y = int(num)
+if y % 11 == 0:
+    square = y * y
+    print("square of the number :", square)
+else:
+    print("ths number not divisible by 11")
+
+#7). Python program to check given number is odd or even.
+
+num = input("enter number: ")
+x = int(num)
+if x % 2 == 0:
+    print ("Even Number")
+else:
+    print("number is odd")
+"""
+"""
+#Check a number is part of the Fibonacci series
+
+fibnumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+x = input("enter any number between 1 to 30: ")
+num = int(x)
+
+if num  in fibnumbers:
+    print("number is fib number")
+else:
+    print("number is not a Fib number")
+# Python program to check authentication with the given username and password.
+"""
+"""
+username = "nalini"
+pwd = "123456"
+uname = input("enter username:")
+password = input("enter Password")
+if uname == username and password == pwd:
+    print("valid user")
+else:
+    print("invalid user")
+"""
+
+
+
 
 
 
