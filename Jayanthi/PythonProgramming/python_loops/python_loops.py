71a410cc064bf9ea0b8a2767ce07f9dbe0b238df
Jayanthi Ajay <jayanthi.j90@gmail.com>
2025-12-02 21:03:57 +0530

    loops

---
+++ Jayanthi/PythonProgramming/python_loops/python_loops.py
@@ -0,0 +1,27 @@
+# range(start,end,step)
+for i in range(10,20,1):
+    print(i)
+
+print("_"*50)
+
+for i in range(5): # when the start and step is not given
+    print(i)
+
+print("-"*50)
+
+for i in range(5,-5,-1):
+    print(i)
+
+str1='programming'
+for i in str1:
+    print(i)
+print('_'*50)
+list1=[3,5,6,7,8,9]
+for i in list1:
+    print(i)
+
+print('_'*50)
+for i in range(10,15,2):
+    print("Address of i",i)
+    for j in range(5,-8,-1):
+        print('door bell',j)
