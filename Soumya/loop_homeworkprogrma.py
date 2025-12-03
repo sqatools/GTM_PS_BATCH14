cce26e88f9d6741c74a15115447cacf5cda1cbec
soumyagit2025 <sowmya083.bng@gmail.com>
2025-12-03 17:12:00 +0530

    adding file from pycharm

--- Soumya/loop_homeworkprogrma.py
+++ Soumya/loop_homeworkprogrma.py
@@ -1,7 +1,5 @@
-for i in range(1, 6): # i =1, 2, 3, 4, 5
-    # j decide the number of star in each row
-    for j in range(1, 7-i):# (1, 6)(1, 5)(1, 4)(1, 3)(1, 2)
-        print("*", end=" ")
 
-    print()
+
+
+
 
