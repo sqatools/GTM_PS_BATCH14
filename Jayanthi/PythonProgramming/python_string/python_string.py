5f5cbba1fec041e52aa411fe6dceafbc083cad78
Jayanthi Ajay <jayanthi.j90@gmail.com>
2025-12-03 22:59:59 +0530

    String 03/12/2025

--- Jayanthi/PythonProgramming/python_string/python_string.py
+++ Jayanthi/PythonProgramming/python_string/python_string.py
@@ -1,1 +1,17 @@
 # String formating
+name = input("Enter the name ")
+age= int(input("Enter the age "))
+city = input("Enter the city ")
+
+#concat
+result1="my name is "+name+" and my age "+str(age)+" living in "+city
+print(result1)
+
+#format method
+result2="my name is {} and my age {} living in {}".format(name,age,city)
+print(result2)
+
+#f format method
+result3=f"my name is {name} and my age {age} living in {city}"
+print(result3)
+
