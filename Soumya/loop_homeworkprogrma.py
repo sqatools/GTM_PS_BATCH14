9cb6aae63d6f0bf4c80eeadd8ff9b77ba963f44b
soumyagit2025 <sowmya083.bng@gmail.com>
2025-12-03 18:16:12 +0530

    adding file from pycharm

--- Soumya/loop_homeworkprogrma.py
+++ Soumya/loop_homeworkprogrma.py
@@ -1,3 +1,118 @@
+"""
+Pattern program
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*", end=" ")
+
+    print()
+"""
+
+
+
+
+"""
+    
+    for i in range(1,6):
+    for j in range(1,7-i):
+        print("*", end=" ")
+
+    print()
+
+    * * * * *
+    * * * *
+    * * *
+    * *
+    *
+"""
+
+"""
+
+for i in range(7):
+    if i==0 or i==6:
+        print("  * * *")
+    else :
+        print("*" + " " * 7 + "*")
+        
+for i in range(1, 6):
+    if i == 1 or i == 5:
+        print("  * * *")
+    else:
+        print("*", "", "", "", "")
+        
+ * * *
+*       *
+*       *
+*       *
+*       *
+*       *
+  * * *
+
+
+"""
+
+
+
+
+"""
+for i in range(1, 6):
+    for j in range(1, 6):
+        print("*", end=" ")
+    print()
+    
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+"""
+
+
+
+
+
+
+"""
+for i in range(1, 6):
+    if i == 1 or i == 5:
+        print("  * * *")
+    else:
+        print("*", "*", "*", "*", "*")
+        
+  * * *
+* * * * *
+* * * * *
+* * * * *
+  * * *
+  
+"""
+
+for i in range(1, 6):
+    if i == 1 or i == 5:
+        print("  * * *")
+    else:
+        print("*", "", "", "", "   *")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
