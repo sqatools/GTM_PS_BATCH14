a763afed76974340f0505e755f0289cdc4c0d27e
nalinimajeti <nmajeticollegecraft@gmail.com>
2025-12-08 14:48:46 +0530

    simple Python Programs

---
+++ Nalini/PythonPrograms/python_strings.py
@@ -0,0 +1,13 @@
+str = "Python Programming"
+print(str[0:6])
+print(str[-11:-7])
+print(str[-5:-3])
+
+str="pyton"
+batch=14
+str = "we are in {}  {} th batch".format(str,batch)
+print (str)
+print(len(str))
+name = "Nalini"
+print(name.index("n"))
+print(name.index("N"))
