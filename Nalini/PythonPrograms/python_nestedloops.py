4e0ff7be89004ffa9837c05a03367ad4c69a3595
nalinimajeti <nmajeticollegecraft@gmail.com>
2025-12-08 11:17:30 +0530

    simple Python Programs

--- Nalini/PythonPrograms/python_nestedloops.py
+++ Nalini/PythonPrograms/python_nestedloops.py
@@ -7,11 +7,19 @@ for i in range(6, -1, -1):
     print()
 
 """
-for i in range(1, 6):
-    for j in range(i, 5):
-        if i in [1, 5]:
-            print(" ")
+for i in range(1, 7):
+    for j in range(1, 6):
+        if i in [1, 6]:
+            if j in [1, 5]:
+                print(" ", end=" ")
+            else:
+                print("*", end=" ")
+        elif i in [2, 3, 4, 5]:
+            if j in [1, 5]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
         else:
-            print(" * ", end=" ")
+            print(j, end=" ")
 
-print( )
+    print()
