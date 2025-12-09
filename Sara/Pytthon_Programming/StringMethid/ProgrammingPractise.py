d0a9c0e5a8c2e052100f27b1939cfa333c6a6497
saradcunha008-code <dcunha.sara@gmail.com>
2025-12-10 00:14:50 +0530

    practise done for 3 programs

--- Sara/Pytthon_Programming/StringMethid/ProgrammingPractise.py
+++ Sara/Pytthon_Programming/StringMethid/ProgrammingPractise.py
@@ -20,3 +20,37 @@ for char in s1:
     else:
         continue
 print("Total vowels using 2nd way:", count) # TTotal vowels using 2nd way: 10
+
+print()
+##################################################################
+# Write a python program to remove duplicate char from given string
+print("-"*15, "Program 2","-"*15)
+s1="We are learning python programming"
+output=""
+# output- We arlnigpythom
+for char in s1:
+    if char in output:
+        continue
+    else:
+        output= output+char
+print(output) # We arlnigpythom
+
+print()
+##################################################################
+# Write a python program to remove duplicate char from given string
+print("-"*15, "Program 3","-"*15)
+s1="Hello Hello People. Good Morning People"
+output1=""
+s2= s1.split(" ")
+print(s2)
+for char in s2:
+    if char in output1:
+        continue
+    else:
+        output1=output1+char+" "
+print(output1) #Hello People. Good Morning
+
+print()
+##################################################################
+# Write a program to get the largest word from the given statement 44:11
+
