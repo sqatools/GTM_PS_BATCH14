4f62791e1a7a90556aaaea338d9e2b8195559a8a
saradcunha008-code <dcunha.sara@gmail.com>
2025-12-10 23:30:25 +0530

    list method cloass 2 prac

--- Sara/Pytthon_Programming/Part6_List/Class2_ListMethod.py
+++ Sara/Pytthon_Programming/Part6_List/Class2_ListMethod.py
@@ -64,4 +64,120 @@ print(l1) # [1, 33, 24, 100]
 s2=l1.pop(-2)
 print(s2) # 24
 
+print()
+# ************* sort Method ***************
+print('-' * 15, 'Sort Method', '-' * 15)
+l1=[33,2,4,12,0,34]
+l1.sort()
+print("value in ascending order: ", l1) # value in ascending order:  [0, 2, 4, 12, 33, 34]
 
+l2=[33,2,4,12,0,34]
+l2.sort(reverse=True)
+print("value in descending order: ", l2) # value in descending order:  [34, 33, 12, 4, 2, 0]
+
+l3=['Are','python','Program','clock']
+l3.sort()
+print("Ascending order :", l3) # Ascending order : ['Are', 'Program', 'clock', 'python']
+
+l4=['Hello','Python','ant','we']
+l4.sort(reverse=True)
+print("Descending order:", l4) # Descending order: ['we', 'ant', 'Python', 'Hello']
+
+'''
+l5=['Check',2,55,'april',34]
+l5.sort(reverse=True)
+print("Ascending alphanumeric:", l5) # no output for ascending and descending
+'''
+
+print()
+# ************* Reverse Method ***************
+print('-' * 15, 'Reverse Method', '-' * 15)
+l1=[3,44,2,53,36,123]
+l1.reverse()
+print("Reversed number list is:",l1) # Reversed number list is: [123, 36, 53, 2, 44, 3]
+
+l2=['Are','python','Program','clock']
+l2.reverse()
+print("Reversed alphabet list is:",l2) # Reversed alphabet list is: ['clock', 'Program', 'python', 'Are']
+
+print()
+# ************* Count Method ***************
+print('-' * 15, 'Count Method', '-' * 15)
+l1=[1,2,33,2,456,2,55,2]
+r1=l1.count(2)
+print("Counting total 12 by storing in var:",r1) # Counting total 12 by storing in var: 4
+print("count of 12 directly:", l1.count(2)) # count of 12 directly: 4
+'''
+r1=l1.count(l1,33)
+print(r1) #blank screen
+'''
+l2= ['Are','clock','python','Clock','Program','clock']
+print('Var count:', l2.count('clock')) # Var count: 2
+print('Capital Var count:', l2.count('Clock')) # Capital Var count: 1
+print('single char count:', l2.count('y')) # single char count: 0
+
+print()
+# ************* Index Method ***************
+print('-' * 15, 'Index Method', '-' * 15)
+l1=[4,2,44,'clock',2,'Python']
+r1=l1.index(2)
+print('Index value by storing in var:', r1) # Index value by storing in var: 1
+print('Index value directly',l1.index('Python')) # Index value directly 5
+
+print()
+# ************* clear Method ***************
+print('-' * 15, 'clear Method', '-' * 15)
+l1=[4,2,44,'clock',2,'Python']
+print("Cleared list:", l1.clear()) # Cleared list: None
+
+print()
+# ************* Lisr concatenation Method ***************
+print('-' * 15, 'concatenation Method', '-' * 15)
+l1=[1,2,3]
+l2=['are',44,'Python',2]
+l3=l1+l2
+print(l3) # [1, 2, 3, 'are', 44, 'Python', 2]
+print(l1) # [1, 2, 3]
+print(l2) # ['are', 44, 'Python', 2]
+
+print()
+# ************* List repetition  ***************
+print('-' * 15, 'repetition Method', '-' * 15)
+l1=[1,2,3]
+l3= l1*2
+print(l3) # [1, 2, 3, 1, 2, 3]
+
+l4=[1,33,'python',4,'we']
+print(l4*2) # [1, 33, 'python', 4, 'we', 1, 33, 'python', 4, 'we']
+
+print()
+# ************* Max, Min, Sum  ***************
+print('-' * 15, ' Max, Min, Sum Function', '-' * 15)
+l1=[22,33,4,564,234,2]
+print("Max value:", max(l1)) # Max value: 564
+print("Min value:", min(l1)) # Min value: 2
+print("Sum total:", sum(l1)) # Sum total: 859
+
+print('-'*20)
+l2=['Python','are','We','April']
+print("Max value:", max(l2)) # Max value: are
+print("Min value:", min(l2)) # Min value: April
+#print("Sum total:", sum(l2)) # Empty screen
+
+print()
+for i in range(65, 91):
+    print(chr(i), end=" ")
+# A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+print()
+
+for j in range(97, 123):
+    print(chr(j), end=" ")
+
+# a b c d e f g h i j k l m n o p q r s t u v w x y z
+
+print()
+# *************ord function  ***************
+print('-' * 15, ' ord Function', '-' * 15)
+
+# ord() function :  this function return ASCII of value of character
+print("ASCII of a:", ord('a'))  # ASCII of a: 97
