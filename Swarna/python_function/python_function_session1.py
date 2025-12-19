b70c001a0939c0c3f724b839ccba5523dd9111d7
swarna <tripathy.swarna7@gmail.com>
2025-12-19 17:07:31 +0000

    Practice python function program

---
+++ Swarna/python_function/python_function_session1.py
@@ -0,0 +1,12 @@
+# function without parameter
+def greeting():
+    print("Hello!")
+
+greeting()
+greeting()
+
+# function with parameter
+def greeting_msg(a):
+    print("+" * 20)
+    print(a)
+    print("$"*20)
