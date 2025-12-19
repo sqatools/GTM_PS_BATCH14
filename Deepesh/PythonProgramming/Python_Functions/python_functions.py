a97900f65b0a6fb413628c62d4b3017600c634cc
sqatools <sqatools.automation@gmail.com>
2025-12-19 22:11:57 +0530

    Python Function2

--- Deepesh/PythonProgramming/Python_Functions/python_functions.py
+++ Deepesh/PythonProgramming/Python_Functions/python_functions.py
@@ -117,3 +117,121 @@ addition_values(10, 20)
 
 addition_values(A=20, B=50, D=70)
 # addition : 170
+
+print("_"*50)
+###############################################
+# *args parameter to the function.
+
+def get_square_of_values(*args):
+    """
+    *args accepts values in tuple format, user can provide n number of values with
+    args parameter.
+    :param args: this param accept n number value while
+    :return:
+    """
+    print(args, type(args))
+    for val in args:
+        #print(val, ":", val**2)
+        print(val, type(val))
+
+
+get_square_of_values(4, 6, 7, 8, 12)
+get_square_of_values(10, 20, 30)
+get_square_of_values(5, 5.5, 'Python', [4, 5, 7], {'a': 123}, {5, 6, 73}, True)
+
+
+
+print("_"*50)
+###############################################
+# **kwargs parameter to the function.
+
+def user_details(**kwargs):
+    """
+    kwargs accepts the value in the key value and store as dictionary
+    :param kwargs:
+    :return:
+    """
+    print(kwargs, type(kwargs))
+
+    for k, v in kwargs.items():
+        print(k ,":", v)
+
+
+user_details(name='Rahul', age=30, email='rahul@gmail.com', phone=5454655667)
+#user_details(6, 7, 8, 9)
+
+
+def verify_login(**kwargs):
+    db_user = 'user1'
+    db_pass = 'user@1234'
+    if kwargs['username'] == db_user and kwargs['password'] == db_pass:
+        print("Login Successful")
+    else:
+        print("Access denied")
+
+print("_"*50)
+#verify_login(username='user2', password='user@1234')
+#verify_login(username='user1', password='user@1234')
+
+#verify_login()
+
+print("_"*50)
+###########################################
+# return statement in the function.
+
+def get_factorial(num):
+    fact = 1
+    for i in range(num, 0,  -1):
+        fact = fact*i
+
+    return fact
+
+
+result = get_factorial(6)
+print("factorial result :", result)
+
+# return statement can stop the execution of the function
+def addition_of_values(num):
+    add = 0
+    for i in range(num):
+        print(i)
+        if add > 30:
+               return add
+
+        add = add + i
+
+    print("Execution completed")
+
+
+print("_"*50)
+result_add = addition_of_values(20)
+print("addition result :", result_add)
+
+
+
+# return multiple values from function
+def math_operations(n1, n2, n3):
+    add = n1+n2
+    mul = n2*n3
+    sub = n3-n1
+    return add, mul, sub
+
+print("_"*50)
+result = math_operations(10, 20, 30)
+print("result :", result)  # (30, 600, 20)
+
+a, b, c = math_operations(50, 100, 20)
+print("addition :", a) # addition : 150
+print("multiplication :", b) # multiplication : 2000
+print("subtraction :", c) # subtraction : -30
+
+
+def math_operation_With_kwargs(**kwargs):
+    add = kwargs['n1'] + kwargs['n2']
+    mul =  kwargs['n2'] *  kwargs['n3']
+    sub =  kwargs['n3'] -  kwargs['n1']
+    return add, mul, sub
+
+
+p, q, r = math_operation_With_kwargs(n1=40, n2=60, n3=70)
+print(p, q, r) # 100 4200 30
