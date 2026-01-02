fc37b1738a0abfaa22b8be97ea4575981ba8896c
DeepeshGit25 <satish_paliwal03@yahoo.co.in>
2026-01-02 19:27:01 +0530

    python_function

--- Satish/PythonProgramming/Python_Functions/module1.py
+++ Satish/PythonProgramming/Python_Functions/module1.py
@@ -7,3 +7,14 @@ def multiply(a,b):
 
 def greeting():
     return "Hello!"
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+       #  return 5*4*3*2*1
+
+result=factorial(5)
+print(result)
