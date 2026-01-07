6a992c1e35195bd5cb3709f71533553c7fdc9d15
NithyaGit25 <tsnithu@gmail.com>
2026-01-07 22:17:07 +0530

    Exception

--- Nithya/Functions/Exceptionhandling/Exceptionhandling.py
+++ Nithya/Functions/Exceptionhandling/Exceptionhandling.py
@@ -5,4 +5,22 @@ def addition(a, b):
     except TypeError:
         return "Error: Both inputs must be numbers."
     
-addition(5,'a')   
+output = addition(5,'a')  
+print(output)  # Output: Error: Both inputs must be numbers.
+
+
+"Multiple Exceptions"
+
+def multiple(a,b):
+    try:
+        print("Multiplication of values are:", a,b)
+        result = a * b
+        except TypeError:
+            return "Error: Both inputs must be numbers."
+        except Exception as e:
+            return f"An unexpected error occurred: {e}"
+
+
+Result= multiple(2,"Test")
+print(Result)
+    
