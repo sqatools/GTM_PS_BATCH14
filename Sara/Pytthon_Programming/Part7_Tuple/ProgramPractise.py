d9b01b8f808bfb6e3b73811ba6ac9baee4b77964
saradcunha008-code <dcunha.sara@gmail.com>
2026-01-09 20:27:16 +0530

    Dictionary message

--- Sara/Pytthon_Programming/Part7_Tuple/ProgramPractise.py
+++ Sara/Pytthon_Programming/Part7_Tuple/ProgramPractise.py
@@ -10,4 +10,12 @@ for i in range (len(t1)):
             continue
 print("Results: ",result) # Results:  [(7, 3), (8, 2), (1, 9)]
 
+print('*'*50)
+##########################################################
+
+# program to get all even numbers/values from tuple
+t2= (3,4,2,6,5,7,8)
+r1=tuple(x for x in t2 if x%2==0)
+print('Even values: ', r1) # Even values:  (4, 2, 6, 8)
+
 
