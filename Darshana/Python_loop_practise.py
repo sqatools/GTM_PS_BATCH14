c75b87af909a1d887ad98716dc6c45a2fb283d5c
darshanamanurkar1010 <darshanamanurkar@gmail.com>
2026-01-21 16:46:51 +0400

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/misc.xml

--- Darshana/Python_loop_practise.py
+++ Darshana/Python_loop_practise.py
@@ -1,10 +1,33 @@
 #apply loop on string
-from sqlite3 import ProgrammingError
 
-str = "Programming2"
-print(str)
+# str = "Programming2"
+# print(str)
+#
+# for char in str:
+#
+#     print(char+ str)
 
-for char in str:
 
-    print(char)
+# num = 1
+# while num <= 10:
+#     print(num)
+#     num = num + 1
+#
 
+# num1 = 1
+# while num1 <= 20:
+#     if num1 % 2 != 0:
+#         print(num1)
+#         num1 +=1
+
+
+# count = 0
+# while count < 8:
+#     print(count)
+#     print(count + 1)
+#
+#     count += 1
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
