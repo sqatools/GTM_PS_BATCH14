dbfacf11ccf0f51dcdb1d5e437e9e69eada76d78
saradcunha008-code <dcunha.sara@gmail.com>
2026-01-29 22:17:19 +0530

    Practise and Notes of class 1 pytest

--- Sara/PyTest _Sessions/Test_Class1.py
+++
@@ -1,8 +0,0 @@
-"""
-Docstring for Sara.PyTest _Sessions.Test_Class1
-
-For pytest test case or function need to start with 'test_'
-if you do not follow this convention pytest will not be able 
-to detect and run your test cases.
-
-"""
