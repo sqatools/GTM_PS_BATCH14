fadcfb08a834ab500ff704dc92f6317a873ee70c
sqatools <sqatools.automation@gmail.com>
2026-02-09 22:58:07 +0530

    pytest conftest file

--- Deepesh/PytestPractice/SeleniumwithPytest/conftest.py
+++ Deepesh/PytestPractice/SeleniumwithPytest/conftest.py
@@ -16,6 +16,7 @@ def get_instance_request(request):
     driver_instance.get("https://sqatools.in/automation-practice-page/")
     driver_instance.maximize_window()
     driver_instance.implicitly_wait(15)
+    # Assigning the driver instance to the class variable
     request.cls.driver = driver_instance
     yield 
     driver_instance.quit()
