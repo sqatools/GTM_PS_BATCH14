74272364b5c8f7d08c860023742982abb80751f3
NithyaGit25 <tsnithu@gmail.com>
2026-02-11 22:07:34 +0530

    pull assignment

---
+++ Nithya/Selenium Basic/WAIT_prgm.py
@@ -0,0 +1,6 @@
+"""
+Implicit wait in Selenium - This will wait for the element to be present in the DOM for a certain amount of time before throwing an exception.
+Explicit wait in Selenium - This will wait for a specific condition to be met before proceeding.
+Fluent wait in Selenium - This is a more flexible version of explicit wait, allowing you to define polling frequency and timeout.
+
+"""
