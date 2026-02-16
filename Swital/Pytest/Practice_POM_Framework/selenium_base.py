c0d3914c37b323f817d0e2026d0b55e2ef77c91a
Swital <switalmacwan2207@gmail.com>
2026-02-16 21:05:06 +0530

    POM Framework Practice

---
+++ Swital/Pytest/Practice_POM_Framework/selenium_base.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+class SeleniumBase:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 20)
+    def get_element(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
+    def click_element(self, locator):
+        element = self.get_element(locator)
+        element.click()
+    def enter_text(self, locator, text):
+        element = self.get_element(locator)
+        element.clear()
+        element.click(text)
+    def get_text(self, locator):
+        element = self.get_element(locator)
+        return element.text
+            
