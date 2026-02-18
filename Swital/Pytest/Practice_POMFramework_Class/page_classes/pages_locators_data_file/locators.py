139833deb220c14f1459b217fcd70fb3f95d67b8
Swital <switalmacwan2207@gmail.com>
2026-02-18 22:42:25 +0530

    Elements interaction on pom

--- Swital/Pytest/Practice_POMFramework_Class/page_classes/pages_locators_data_file/locators.py
+++ Swital/Pytest/Practice_POMFramework_Class/page_classes/pages_locators_data_file/locators.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
+
 class Locator:
     F_name = (By.XPATH, "//span[contains(text(), 'First Name')]/following-sibling::input[@name = 'firstname']")
     L_name = (By.XPATH, "//span[contains(text(), 'Last Name')]/following-sibling::input[@name = 'firstname']")
     radio_gender = (By.ID, "female")
     no_passenger = (By.ID, "admorepass")
+    multicity_checkboxes = (By.XPATH, "//th[text()='City Name']/parent::tr/following-sibling::tr//input")
+
     
