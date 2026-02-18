82a83c920056a090ed35506f94cdd7e26d666a68
sqatools <sqatools.automation@gmail.com>
2026-02-18 22:33:58 +0530

    added code

--- Deepesh/PytestPractice/AutomationFramework/base/selenium_base.py
+++ Deepesh/PytestPractice/AutomationFramework/base/selenium_base.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 class SeleniumBase:
     def __init__(self, driver):
@@ -48,3 +49,38 @@ class SeleniumBase:
         """
         element = self.get_element(locator)
         return element.text
+
+    def is_element_selected(self, locator):
+        """
+        Docstring for is_element_selected
+        :param self: Description
+        :param locator: locator value will provide in tuple format (By.ID, "id_value")
+        :return: True if element is selected, False otherwise
+        """
+        element = self.get_element(locator)
+        return element.is_selected()
+    
+
+    def select_dropdown_option(self, dropdown_locator, option_text):
+        """
+        Docstring for select_dropdown_option
+        :param self: Description
+        :param dropdown_locator: locator value for the dropdown element in tuple format (By.ID, "id_value")
+        :param option_text: visible text of the option to be selected
+        :return: None
+        """
+        dropdown_element = self.get_element(dropdown_locator)
+        select = Select(dropdown_element)
+        select.select_by_visible_text(option_text)
+
+
+    def upload_file(self, locator, file_path):
+        """
+        Docstring for upload_file
+        :param self: Description
+        :param locator: locator value for the file input element in tuple format (By.ID, "id_value")
+        :param file_path: absolute path of the file to be uploaded
+        :return: None
+        """
+        file_input_element = self.get_element(locator)
+        file_input_element.send_keys(file_path)
