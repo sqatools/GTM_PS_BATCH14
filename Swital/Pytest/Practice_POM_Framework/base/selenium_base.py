ebcb72d7d94ff841ca7fda423e56d5b4a3559e87
Swital <switalmacwan2207@gmail.com>
2026-02-19 20:04:51 +0530

    dictionary sqa programs practice

--- Swital/Pytest/Practice_POM_Framework/base/selenium_base.py
+++ Swital/Pytest/Practice_POM_Framework/base/selenium_base.py
@@ -4,15 +4,19 @@ class SeleniumBase:
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(self.driver, 20)
+
     def get_element(self, locator):
         return self.wait.until(EC.presence_of_element_located(locator))
+    
     def click_element(self, locator):
         element = self.get_element(locator)
         element.click()
+
     def enter_text(self, locator, text):
         element = self.get_element(locator)
         element.clear()
-        element.click(text)
+        element.send_keys(text)
+        
     def get_text(self, locator):
         element = self.get_element(locator)
         return element.text
--- Swital/Pytest/Practice_POM_Framework/page_classes/pages/login_page.py
+++ Swital/Pytest/Practice_POM_Framework/page_classes/pages/login_page.py
@@ -1,14 +1,18 @@
-from selenium_base import SeleniumBase
-from page_locators import Page_locator
+from ...base.selenium_base import SeleniumBase
+from .page_locators import Page_locator
 class Login_page(SeleniumBase):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
+
     def launch_site(self, url):
         return self.driver.get(url)
+    
     def enter_first_name(self, value):
-        self.driver(Page_locator.F_name, value)
+        self.enter_text(Page_locator.F_name, value)
+
     def enter_last_name(self, value):
-        self.driver(Page_locator.L_Name, value)
+        self.enter_text(Page_locator.L_Name, value)
+
     def enter_address(self, value):
-        self.driver(Page_locator.Address, value)
+        self.enter_text(Page_locator.Address, value)
