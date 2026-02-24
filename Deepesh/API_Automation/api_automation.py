cc906dc3aa77c0b2bfeb6511501b3f0d994ac19e
sqatools <sqatools.automation@gmail.com>
2026-02-24 22:42:12 +0530

    added api code in framework

--- Deepesh/API_Automation/api_automation.py
+++ Deepesh/API_Automation/api_automation.py
@@ -27,6 +27,7 @@ API Status Code :
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status#information_responses
 
 """
+# requests method help to execute all https methods and return response
 import requests
 # pip install requests
 import json
@@ -79,8 +80,36 @@ def add_new_object():
     print(response.status_code)
     print(response.json())
 
-add_new_object()
+#add_new_object()
 """
 200
 {'id': 'ff8081819c5368bb019c8b7f5c385797', 'name': 'Apple MacBook Pro 16', 'createdAt': 1771866905656, 'data': {'year': 2019, 'price': 1849.99, 'CPU model': 'Intel Core i9', 'Hard disk size':
 """
+
+def create_new_details():
+   
+
+    url = "https://gorest.co.in/public/v2/users"
+
+    payload = json.dumps({
+    "name": "Sunil Kumar Ramakrishna",
+    "gender": "male",
+    "email": "sunil.ramakrishna123@15ce.com",
+    "status": "active"
+    })
+    headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ad6b849f911b220a37b1218280ce88bfa087d8c8f82bee717fd58a47d40f1277'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.json())
+    print(response.status_code)
+
+
+create_new_details()
+# create_new_details()
+# {"id":8384278,"name":"Tenali Kumar Ramakrishna","email":"tenali.ramakrishna123@15ce.com","gender":"male","status":"active"}
+# {'id': 8384282, 'name': 'Sunil Kumar Ramakrishna', 'email': 'sunil.ramakrishna123@15ce.com', 'gender': 'male', 'status': 'active'}
