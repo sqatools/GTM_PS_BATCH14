9a85cf7d56bad24477e1c7d271b01fbf08eca15d
saradcunha008-code <dcunha.sara@gmail.com>
2026-03-10 11:01:59 +0530

    locators ID prac

---
+++ Sara/Selenium/locatorsprac1.py
@@ -0,0 +1,13 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver= webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("https://sqatools.in/login-page/")
+driver.implicitly_wait(10)
+
+driver.find_element(By.ID,"email").send_keys("user1@gmail.com")
+driver.find_element(By.ID,"pass").send_keys("User@1234")
+driver.find_element(By.ID,"loginbutton").click()
