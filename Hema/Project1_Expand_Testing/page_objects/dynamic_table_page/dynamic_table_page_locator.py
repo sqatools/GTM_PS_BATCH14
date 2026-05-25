1e4cbca88fed2af83e3b5e4964907bc3c84756cb
Hema Parkhande <hemag.gade@gmail.com>
2026-05-25 23:13:39 +0200

    adding changes

--- Hema/Project1_Expand_Testing/page_objects/dynamic_table_page/dynamic_table_page_locator.py
+++ Hema/Project1_Expand_Testing/page_objects/dynamic_table_page/dynamic_table_page_locator.py
@@ -13,3 +13,9 @@ class DynamicTablePageLocator:
         By.XPATH, "//table[@class='table table-striped']//tbody/tr[td[text()='Chrome']]")
 
     chrome_cpu_atbottom_locator = (By.XPATH, "//p[@class='bg-warning p-1']")
+    chrome_cpu_atbottom_locator2 = (
+        By.XPATH, "//p[contains(text(), 'Chrome CPU')]")
+    chrome_cpu_atbottom_locator3 = (
+        By.XPATH, "//p[contains(text(), 'Chrome CPU') and contains(@class, 'bg-warning')]")
+    chrome_cpu_atbottom_locator4 = (
+        By.XPATH, "//p[contains(text(), 'Chrome CPU') and contains(@class, 'bg-warning') and contains(@class, 'p-1')]")
